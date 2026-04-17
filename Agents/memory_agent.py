a8ceee548b98c3d9cf89562f1fdbdd60f711022c
kir <117801071+kiruthiiga@users.noreply.github.com>
2026-04-17 17:03:56 +0530

    Update memory_agent.py

--- Agents/memory_agent.py
+++ Agents/memory_agent.py
@@ -1,1 +1,17 @@
+import json
+import os
+
+MEMORY_FILE = "memory/history.json"
+
+def save_result(result):
+    history = []
+
+    if os.path.exists(MEMORY_FILE):
+        with open(MEMORY_FILE, "r") as f:
+            history = json.load(f)
+
+    history.append(result)
+
+    with open(MEMORY_FILE, "w") as f:
+        json.dump(history, f, indent=4)
 
